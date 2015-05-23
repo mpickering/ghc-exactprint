15fe907d48f423d3509d1f144e99214c5442ebef
Matthew Pickering <matthewtpickering@gmail.com>
2015-05-23 12:29:24 +0100

    Add failing test

--- tests/examples/NestedLambda.hs
+++ tests/examples/NestedLambda.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE ImplicitParams, NamedFieldPuns, ParallelListComp, PatternGuards #-}
 
 
 getPath :: [String] -> Filter
@@ -6,3 +6,7 @@ getPath names elms =
   let follow = foldl (\f n -> \els-> subElems n $ f els) id' names :: Filter
       id' = id :: Filter
   in  follow elms
+
+match a =
+  case ?pat of
+    POr ps -> choice flips $ map (\p -> \b -> let ?pat = p in match s{ flips = b }) ps
